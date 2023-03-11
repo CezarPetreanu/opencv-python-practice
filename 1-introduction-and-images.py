94a2fac5278f98c409ade09bbdf4071630b4cfc0
CezarPetreanu <cezarpetreanu4013@gmail.com>
2023-03-11 13:48:09 +0200

    #1 - Introduction & Images
    source: https://youtu.be/qCR2Weh64h4

---
+++ 1-introduction-and-images.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread('assets/pisica.jpg', 1)
+# -1 = imagine color, fara transparenta
+#  0 = imagine alb-negru
+#  1 = imagine color, transparenta
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+#img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+cv2.imwrite('pisica.jpg', img)
+cv2.imshow('Imagine', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
